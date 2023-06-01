4600ec98bfd181c5f122cf2aa4d3e330c00415a1
Amiq Dian <66551148+amiqyolo@users.noreply.github.com>
2023-06-01 14:17:49 +0700

    add auth activity and fragment, navigation auth

--- app/src/main/java/com/aplimelta/coffeebidapp/AuthActivity.kt
+++ app/src/main/java/com/aplimelta/coffeebidapp/AuthActivity.kt
@@ -2,18 +2,16 @@ package com.aplimelta.coffeebidapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.aplimelta.coffeebidapp.databinding.ActivityMainBinding
+import com.aplimelta.coffeebidapp.databinding.ActivityAuthBinding
 
-class MainActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
 
-    private val binding: ActivityMainBinding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
+    private val binding: ActivityAuthBinding by lazy {
+        ActivityAuthBinding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-
     }
 }
