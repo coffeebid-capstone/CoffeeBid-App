22396a3977a8ac21a78466b934888c1dc84fee74
Amiq Dian <66551148+amiqyolo@users.noreply.github.com>
2023-06-02 20:02:32 +0700

    implement UI home

--- app/src/main/java/com/aplimelta/coffeebidapp/ui/main/activity/MainActivity.kt
+++ app/src/main/java/com/aplimelta/coffeebidapp/ui/main/activity/MainActivity.kt
@@ -2,6 +2,9 @@ package com.aplimelta.coffeebidapp.ui.main.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.aplimelta.coffeebidapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +17,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(androidx.navigation.fragment.R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
 
+//        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
